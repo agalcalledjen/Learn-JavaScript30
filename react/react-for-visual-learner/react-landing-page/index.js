9da7024b61e713308beaffb21f6fc04d0e3e8aec
nejmal <lam.jennifer.ky@gmail.com>
2019-01-06 21:39:36 -0800

    Init

---
+++ react/react-for-visual-learner/react-landing-page/index.js
@@ -0,0 +1,12 @@
+class App extends React.Component {
+  render() {
+    return (
+      <div> Hello React via a project! </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
