b53b6848830ecb9b97230374c6c4c3799f224ffb
nejmal <lam.jennifer.ky@gmail.com>
2019-01-10 21:20:19 -0800

    Add react-grid

---
+++ react/react-for-visual-learner/react-landing-page/components/villages/neighborhoods/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Container, Row, Col } from "react-grid-system";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <section className="footer">
+        <Container>
+          <Row>
+            <Navigation />
+          </Row>
+        </Container>
+      </section>
+    );
+  }
+}
+
+export default Footer;
