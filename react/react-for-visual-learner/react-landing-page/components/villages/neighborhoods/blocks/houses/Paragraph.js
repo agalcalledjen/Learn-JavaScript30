fbda0e58a1f5f7c229257deeab3417edb89e9595
nejmal <lam.jennifer.ky@gmail.com>
2019-01-13 22:35:12 -0800

    Add branding svg.

---
+++ react/react-for-visual-learner/react-landing-page/components/villages/neighborhoods/blocks/houses/Paragraph.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Paragraph extends React.Component {
+  render() {
+    return (
+      <div>
+        <p className="paragraph">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vo\
+          lutpat, elit pulvinar aliquam convallis, libero mauris rhoncus erat,
+          non porttit\ or lectus justo et ex. In hac habitasse platea dictumst.
+        </p>
+      </div>
+    );
+  }
+}
---
+++ react/react-for-visual-learner/react-landing-page/components/villages/neighborhoods/blocks/houses/Title.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Title extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="title">Developer Bot for Slack</h1>
+      </div>
+    );
+  }
+}
